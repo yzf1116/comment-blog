5c250113e2e7f69928c0e20295071b35c59efe31
Ekko <770817172@qq.com>
2017-09-11 14:03:36 +0800

    comment

---
+++ src/CommentInput.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class CommentInput extends Component {
+	static propTypes = {
+		onSubmit: PropTypes.func
+	}
+	constructor (){
+		super();
+		this.state = {
+			username: '',
+			content: ''
+		}
+	}
+	componentDidMount(){
+		this.textarea.focus()
+	}
+	componentWillMount(){
+		this._loadUsername()
+	}
+	_loadUsername(){
+		const username = localStorage.getItem('username')
+		if(username){
+			this.setState({username})
+
+		}
+	}
+	_saveUsername (username){
+		localStorage.setItem('username',username)
+
+	}
+	handleUsernameBlur (event){
+		this._saveUsername(event.target.value)
+
+	}
+	handleUsernameChange (event){
+		this.setState({
+			username: event.target.value
+		});
+	}
+	handleContentChange (event){
+		this.setState({
+			content: event.target.value
+		})
+	}
+	handleSubmit (){
+		if (this.props.onSubmit){
+			const {username, content} = this.state;
+			this.props.onSubmit({username,content})
+		}
+		this.setState({content:''})
+	}
+    render() {
+    	return (
+    		<div className="comment-input">
+			    <div className="comment-field">
+				    <span className="comment-field-name">用户名:</span>
+				    <div className="comment-field-input">
+					    <input
+						    value={this.state.username}
+							onBlur={this.handleUsernameBlur.bind(this)}
+					        onChange={this.handleUsernameChange.bind(this)}/>
+				    </div>
+			    </div>
+			    <div className="comment-field">
+				    <span className="comment-field-name">评论内容:</span>
+				    <div className="comment-field-input">
+					    <textarea
+						    ref={(textarea) => this.textarea = textarea}
+						    value={this.state.content}
+					        onChange={this.handleContentChange.bind(this)}/>
+				    </div>
+			    </div>
+			    <div className="comment-field-button">
+				    <button onClick={this.handleSubmit.bind(this)}>
+					    发布
+				    </button>
+			    </div>
+		    </div>
+	    )
+    }
+}
+export default CommentInput
+
+
+
+
+
+
+
+
